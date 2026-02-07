48d1c23cdf26abc8c9ea746c7a5eb1c8f9e61af3
Dekim-49 <chernyadeva.anna@mail.ru>
2026-02-07 12:47:56 +0300

    Написан рекурсивный метод

--- main.cpp
+++ main.cpp
@@ -28,54 +28,6 @@ void CreateSplineCurve(MbCurve3D* *result, SArray<MbCartPoint3D> points, MbeSpac
 	SplineCurve(points, false, type, *result);
 }
 
-//
-//void CreateCurve()
-//{
-//	SArray<MbCartPoint3D> points;
-//	MbCurve3D* result;
-//
-//	// Дуга окружности
-//	/*dots3(&points);
-//	CreateSplineCurve(&result, points, st_Arc3D);*/
-//
-//	
-//	dots7(&points);
-//	CreateSplineCurve(&result, points, st_Bezier3D);
-//	
-//
-//	std::string filePath = "C:\\Users\\ASUS\\Desktop\\example.txt";
-//	std::ofstream outFile(filePath);
-//	if (!outFile.is_open()) {
-//		std::cerr << "Ошибка открытия файла." << std::endl;
-//	}
-//
-//	double len = result->GetParamLength();
-//	double t = 0.0;
-//	for (t = 0.0; t <= len; t += 0.1)
-//	{
-//		MbCartPoint3D pt;
-//		result->PointOn(t, pt);
-//
-//		if (std::abs(pt.x) < std::exp(-16))
-//		{
-//			pt.x = 0.0;
-//		}
-//		if (std::abs(pt.y) < std::exp(-16))
-//		{
-//			pt.y = 0.0;
-//		}
-//		if (std::abs(pt.z) < std::exp(-16))
-//		{
-//			pt.z = 0.0;
-//		}
-//		//std::cout << "t = " << t << " pt = " << pt.x << " " << pt.y << " " << pt.z << std::endl;
-//		outFile <<  "" << pt.x << "; " << pt.y << "" << std::endl;
-//	}
-//
-//
-//	//std::cout << "GetParamLength = " << result->GetParamLength() << "GetMetricLength = " << result->GetMetricLength();
-//
-//}
 
 /// <summary>
 /// Алгоритм линеаризации. Если расстояние от части кривой до ломаной больше радиуса r, то эта часть кривой делится на два
@@ -107,7 +59,7 @@ SArray<MbCartPoint3D> Linearization(MbCurve3D* curve, double r)
 	while (flag)
 	{
 		int i = 0;
-		for (i = 0; i < paramForCoords.Count()-1; i++)
+		for (i = 0; i < paramForCoords.size() - 1; i++)
 		{
 			double tMid = (paramForCoords[i] + paramForCoords[i + 1]) / 2;
 			MbCartPoint3D ptCenter = curve->PointOn(tMid);
@@ -128,19 +80,73 @@ SArray<MbCartPoint3D> Linearization(MbCurve3D* curve, double r)
 	return result;
 }
 
+
+
+void RecurLinearize(MbCurve3D* curve, std::vector<double>& params, SArray<MbCartPoint3D>& points, int leftIndex, int rightIndex, double r)
+{
+	if (leftIndex >= rightIndex ) return;
+
+	double tMid = (params[leftIndex] + params[rightIndex]) / 2;
+	MbCartPoint3D ptCenter = curve->PointOn(tMid);
+
+	MbCurve3D* segm;
+	Segment(points[leftIndex], points[rightIndex], segm);
+	double dist = segm->DistanceToPoint(ptCenter);
+	delete segm;
+	if (dist <= r) return;
+
+	params.insert(params.begin() + rightIndex, tMid);
+	points.InsertObj(points[rightIndex], ptCenter);
+
+	int count = points.Count();
+	RecurLinearize(curve, params, points, leftIndex, rightIndex, r);
+	RecurLinearize(curve, params, points, rightIndex + (points.Count() - count), rightIndex + (points.Count() - count) + 1, r);
+
+}
+SArray<MbCartPoint3D> LinearizationRecurse(MbCurve3D* curve, double r)
+{
+	SArray<MbCartPoint3D> result; //массив координат ломаной
+	std::vector <double> paramForCoords; //вектор параметров для нахождения координат 
+
+	double tMin = curve->GetTMin(),
+		tMax = curve->GetTMax();
+
+	//Вносим начало и конец кривой
+	MbCartPoint3D ptStart = curve->GetLimitPoint(1),
+		ptEnd = curve->GetLimitPoint(2);
+
+	
+	paramForCoords.push_back(tMin);
+	paramForCoords.push_back(tMax);
+
+	result.Add(ptStart);
+	result.Add(ptEnd);
+
+	RecurLinearize(curve, paramForCoords, result, 0, result.Count() - 1, r);
+	return result;
+}
+
+
+
+
+
 int main()
 {
 	SArray<MbCartPoint3D> points; //массив коорлинат для линеаризации
 	MbCurve3D* curve; //кривая
 
-	dots7(&points);
+	dots7(&points); //st_Bezier3D
+	//dots3(&points); //st_Arc3D
 	CreateSplineCurve(&curve, points, st_Bezier3D);
 
 
-	points = Linearization(curve, 0.1);
+	points = LinearizationRecurse(curve, 0.1);
+
+
 	for (int i = 0; i < points.Count(); i++)
 	{
-		std::cout << "(" << points[i].x << " ; " << points[i].y << " ; " << points[i].z << std::endl;
+		std::cout << "" << points[i].x << " , " << points[i].y << "" << std::endl;
 	}
+	std::cout << "---------------------------" << std::endl;
 	return 0;
 }
